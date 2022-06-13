f2c7c9dbb62f1f2da1b8443e2b8640b62d72b350
Mattia-Zanini <mattzanini2@gmail.com>
2022-06-13 21:07:36 +0200

    Added traps

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemCollector : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI cherriesText;
+    private int cherries = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            Destroy(collision.gameObject);
+            cherries++;
+            Debug.Log($"Player collected a cherry, cherries counter: {this.cherries}");
+            cherriesText.text = $"Cherries: {this.cherries}";
+        }
+    }
+}
